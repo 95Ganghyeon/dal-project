e201bfd2f811c5f445d9aaef68bcaae06d20cf7a
Ubuntu <toomy0toons@gmail.com>
2020-01-18 02:27:58 +0000

    model, templates 생성

--- dal/dal/settings.py
+++ dal/dal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'to+c7@u(s0m#8ag3^asvpm-f=-^0dm88*&mrid&lzb*7$q1*3x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- dal/website/admin.py
+++ dal/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from website.models import Survey
 
 # Register your models here.
+
+@admin.register(Survey)
+class BookAdmin(admin.ModelAdmin):
+    pass
