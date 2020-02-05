1dd0e55243ec6e903ac22d24b25a7236889a85b0
Dawnpool <ye1219@gmail.com>
2020-02-05 14:39:35 +0900

    changed ranking model

--- dal/ranking/models.py
+++ dal/ranking/models.py
@@ -4,6 +4,9 @@ from product.models import Product
 # Create your models here.
 class RankingBoard(models.Model):
     id = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=4)
+    m_type = models.CharField(max_length=4)
     product_fk = models.OneToOneField(Product, on_delete=models.SET_NULL, null=True)
     ranking = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.m_type + " " + self.product_fk + " " + str(self.ranking)
