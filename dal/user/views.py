d1943b21a9fb82f1059f5c4688968103acc3c06a
toomy0toons <toomy0toons@gmail.com>
2020-02-12 17:59:00 +0900

    change profile model

--- dal/user/views.py
+++ dal/user/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 
+from .models import Profile
 # Create your views here.
 
 @login_required
 def profile(request):
-  m_type = "ISTP"
-  n_result = 4
-  context = {'m_type':m_type, 'n_result':4}
+  print(request.user.id)
+  print()
+  # n_result = profile.cart.product_set.count()
+  context = {'m_type':'ISTP', 'n_result':4}
   return render(request,'profile.html',context=context)
