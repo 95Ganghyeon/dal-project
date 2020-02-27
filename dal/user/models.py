7cf449dc85c3c3a0f6413afde5c6df7d3035d524
95Ganghyeon <kgh9959@naver.com>
2020-02-27 19:13:27 +0900

    user profile model 수정

--- dal/user/models.py
+++ dal/user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 from survey.models import Survey
+from product.models import Product
 from datetime import datetime
 
 class Profile(models.Model):
@@ -10,16 +11,15 @@ class Profile(models.Model):
     nickname = models.CharField(max_length=20, unique=True,)
     birth_date = models.DateField()
     survey_fk = models.OneToOneField(Survey,on_delete=models.CASCADE,null = True, blank=True)
+    myProduct_fk = models.ManyToManyField(Product,on_delete=models.CASCADE,null = True, blank=True)
+    zzimProduct_fk = models.ManyToManyField(Product,on_delete=models.CASCADE,null = True, blank=True)
+
 
     # 유저 나이 계산함수 (리뷰에 삽입됩니다.)
     def age(self):
         age_calculate = datetime.now().year - self.birth_date.year
         return age_calculate + 1
 
-# 찜한 생리대 
-class Cart(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    product = models.ForeignKey('product.Product',on_delete=models.CASCADE,null = True, blank=True)
 
 class Meta:
     db_table = "account_profile"
