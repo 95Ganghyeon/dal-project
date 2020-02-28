612c3ee0b9560e7c7f23047fd218117652767360
129lovely <129lovely@gmail.com>
2020-02-28 20:38:20 +0900

    init board urls.py, views.py

--- dal/board/views.py
+++ dal/board/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+def notice_list(request, pk):
+    return render(request, "notice_list.html", context={})
+
 def notice_detail(request, pk):
     return render(request, "notice_detail.html", context={})
 
+def user_story_list(request, pk):
+    return render(request, "user_story_list.html", context={})
+
 def user_story_detail(request, pk):
     return render(request, "user_story_detail.html", context={})
+
+def user_story_form(request, pk):
+    return render(request, "user_story_form.html", context={})
