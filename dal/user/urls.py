2317d6964bf2de93c5a672e1fe54218463f139f8
toomy0toons <toomy0toons@gmail.com>
2020-03-11 20:21:05 +0900

    로그아웃 플로우 고침, 드랍다운 고침, 프로필 재검사버튼 고침"

--- dal/user/urls.py
+++ dal/user/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('profile/edit', views.edit_profile, name='edit_profile'), 
     path('profile/delete/<int:product_id>', views.delete_myProduct, name='delete_myProduct'),
+
 ]
